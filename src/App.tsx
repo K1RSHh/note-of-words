e91f6e1244178e6627e68e1c6fdd3eaf92cc4376
K1RSHh <mark2jzx100ua@gmail.com>
2026-03-08 19:55:18 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import {Routes, Route} from "react-router-dom";
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+        <Routes>
+            <Route>
+                <App />
+            </Route>
+        </Routes>
+    </div>
+  )
+}
+
+export default App
