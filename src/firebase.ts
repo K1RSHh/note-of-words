d6a297f13061321e941aa2c19361cd9a51cf8ab9
K1RSHh <mark2jzx100ua@gmail.com>
2026-03-31 18:18:56 +0300

    update: site title

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,11 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-console.log(
-  "Firebase Config Project ID:",
-  import.meta.env.VITE_FIREBASE_PROJECT_ID,
-);
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
